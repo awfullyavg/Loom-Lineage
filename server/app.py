c13a58e030f52f11f0a34600396aefd39b8a7059
Bryant Wiggins <topwigit@gmail.com>
2023-07-26 10:03:26 -0400

    completed db

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
-from models import db, Users, Service, Appointment
+from models import db, User, Family, Loom, Event
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -23,3 +23,15 @@ api = Api(app) #may or may not use RESTful
 @app.route('/')
 def home():
     return ''
+
+class UsersById(Resource):
+    def get(self, id):
+        user = User.query.filter(User.id == id).first()
+
+        if not user:
+            return make_response('{error: "user not found"}', 404)
+        
+        return make_response(user.to_dict(), 200)
+    
+api.add_resource(UsersById, '/users/<int:id>')
+        
