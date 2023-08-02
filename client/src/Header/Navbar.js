f0b78829183e5f8dce59b9c1fdf0c09642d32534
Bryant Wiggins <topwigit@gmail.com>
2023-08-02 11:12:43 -0400

    modal pops up when login button clicked

--- client/src/Header/Navbar.js
+++ client/src/Header/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import ReactDOM  from "react-dom";
 import Logo from "./Logo";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import Modal from 'react-modal';
 
 function Navbar () {
-
+    const [open, setOpen] = useState(false);
     const myStyle = {height: '64px'}
+
+    const handleOpen = () => { //Opens Modal when login button clicked
+        setOpen(true)
+    }
+
+    const handleClose = () => { //Closes Modal when cliked anywhere outside the modal
+        setOpen(false)
+    }
 
     return (
         <nav class="relative flex w-full flex-wrap items-center justify-between bg-Ash-Gray py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
@@ -19,6 +30,18 @@ function Navbar () {
                 loading="lazy" />
                 <span class="font-medium dark:text-neutral-200">Loom Lineage</span>
             </a>
+            </div>
+
+            <div>
+                <button className="login-button" onClick={handleOpen}>Login</button>
+                <Modal isOpen={open} onRequestClose={handleClose} contentLabel="Login" >
+                    <h1>Please Login</h1>
+                    <div>
+
+                    </div>
+
+                </Modal>
+                
             </div>
             
         </div>
