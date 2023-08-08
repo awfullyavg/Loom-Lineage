14a59f407a0a46dba92a7306621d1c3dd38d9dd6
Bryant Wiggins <topwigit@gmail.com>
2023-08-08 14:09:40 -0400

    fixed table

--- client/src/FamilyTable.js
+++ client/src/FamilyTable.js
@@ -76,21 +76,9 @@ function FamilyTable({ user, setUser }) {
                     >
                       Children
                     </th>
-                    {/* <th
-                      scope="col"
-                      className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase"
-                    >
-                      Edit
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase"
-                    >
-                      Delete
-                    </th> */}
                   </tr>
                 </thead>
-                {/* <tbody className="divide-y divide-gray-200">
+                <tbody className="divide-y divide-gray-200">
                   {userFamilies.map((family) => (
                     <tr key={family.id}>
                       <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
@@ -111,19 +99,9 @@ function FamilyTable({ user, setUser }) {
                       <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
                         {family.children}
                       </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a className="text-green-500 hover:text-green-700" href="#">
-                          Edit
-                        </a>
-                      </td>
-                      <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <a className="text-red-500 hover:text-red-700" href="#">
-                          Delete
-                        </a>
-                      </td>
                     </tr>
                   ))}
-                </tbody> */}
+                </tbody>
               </table>
             </div>
           </div>
